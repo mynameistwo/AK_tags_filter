e4fad78f4109668daa57c2ff99a70728b993015f
Dos Barcelon <ernestjohnbarcelon@gmail.com>
2021-04-18 15:16:04 +0800

    base program. units are to be added. filter is to be improved.

--- main.py
+++ main.py
@@ -1,25 +1,3 @@
-# import mysql.connector
-class TagFilterLoop:
-    tagCount = 0
-    tags = []
-    while tagCount != 5:
-        search = input('Enter tags here: ')
-        tags.append(search)
-        tagCount += 1
-    for filter in tags:
-        print(filter)
-
-
-class TagLoop:
-    units = {
-        'Exusiai': ['Top Operator', 'Ranged', 'Sniper', 'DPS'],
-        'SilverAsh': ['Top Operator', 'Melee', 'Guard', 'DPS', 'Support']
-    }
-    for operators in units:
-        for affix in units[operators]:
-            print(operators)
-
-
 def remove_duplicates(x):
     return list(dict.fromkeys(x))
 
