46a2c9c5b8355169339899a94a2912d839eeab1d
Dos Barcelon <ernestjohnbarcelon@gmail.com>
2021-04-18 12:28:05 +0800

    fixed loops for programs

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 # import mysql.connector
+class TagFilterLoop:
+    tagCount = 0
+    tags = []
+    while tagCount != 5:
+        search = input('Enter tags here: ')
+        tags.append(search)
+        tagCount += 1
+    for filter in tags:
+        print(filter)
 
-units = {
-    'Exusiai': ['Top Operator', 'Ranged', 'Sniper', 'DPS'],
-    'SilverAsh': ['Top Operator', 'Melee', 'Guard', 'DPS', 'Support']
-}
-tagCount = 0
-tags = []
-while tagCount != 5:
-    search = input('Enter tags here: ')
-    tags.append(search)
-    tagCount += 1
-# for x in tags:
-#    for keys in units.items():
-#        if
+
+class TagLoop:
+    units = {
+        'Exusiai': ['Top Operator', 'Ranged', 'Sniper', 'DPS'],
+        'SilverAsh': ['Top Operator', 'Melee', 'Guard', 'DPS', 'Support']
+    }
+    for operators in units:
+        for affix in units[operators]:
+            print(affix)
